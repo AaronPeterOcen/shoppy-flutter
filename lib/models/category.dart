a5bab46aafbf05cfd52ed3a0ffe1982cc46a5a92
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-15 19:35:12 +0300

    first commit

---
+++ lib/models/category.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// FlutterError
+class Category {
+  const Category({
+
+  });
+
+  final String vegetables;
+  final String fruit;
+  final String meat;
+  final String dairy;
+  final String carbs;
+  final String sweets;
+  final String spices;
+  final String convenience;
+  final String hygiene;
+  final String other;
+}
